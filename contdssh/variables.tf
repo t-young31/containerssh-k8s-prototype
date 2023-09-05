ac5e78984c902275c58fcb71472feb21523f517d
tom <tom.young.316@gmail.com>
2023-09-05 09:25:29 +0100

    fmt

--- contdssh/variables.tf
+++ contdssh/variables.tf
@@ -1,10 +1,10 @@
 variable "auth_server_image" {
-  type = string
+  type        = string
   description = "Full path to the continer image e.g. ghcr.io/octo-cat/repo-name/auth_server"
 }
 
 variable "guest_image" {
-  type = string
+  type        = string
   description = "Image URI to pull for the guest container e.g. ghcr.io/octo-cat/repo-name/guest_image"
-  default = "containerssh/containerssh-guest-image"
+  default     = "containerssh/containerssh-guest-image"
 }
